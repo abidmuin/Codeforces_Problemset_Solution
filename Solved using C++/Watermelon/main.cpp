971d71e940a90e579a9581fb72578e5c4ff7ce98
Abid Hasan Muin <muin.739@gmail.com>
2019-04-23 21:09:59 +0600

    Add files via upload

---
+++ Solved using C++/Watermelon/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int w;
+    cin >> w;
+    if(w>=1 && w<=100){
+            if(w==1 || w==2){
+                cout << "NO" << endl;
+            }
+            else{
+                if(w%2==0)
+                cout << "YES" << endl;
+            else
+                cout << "NO" << endl;
+            }
+
+    }
+    return 0;
+}
