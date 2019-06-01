242e6d44340eba2182a832d38887037da323d245
Abid Hasan Muin <muin.739@gmail.com>
2019-06-01 16:33:56 +0600

    Add files via upload

---
+++ Solved using C#/Wrong Subtraction/Bear and Big Brother/Bear and Big Brother/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://codeforces.com/problemset/problem/791/A
+namespace Bear_and_Big_Brother
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+            string[] token = line.Split(' ');
+            int[] input = Array.ConvertAll(token, int.Parse);
+            int Limak = input[0];
+            int Bob = input[1];
+
+            int count = 0;
+            while (Limak <= Bob)
+            {
+                count++;
+                Limak = 3 * Limak;
+                Bob = 2 * Bob;
+            }
+            Console.WriteLine(count);
+        }
+
+    }
+}
