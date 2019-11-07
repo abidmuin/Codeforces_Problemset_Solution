0c76995868ba21490deebe21a02adee7465b4f53
Abid Hasan Muin <muin.739@gmail.com>
2019-11-07 22:45:03 +0600

    Add files via upload

---
+++ Solved using C++/Mahmoud and Ehab and the even-odd game/main.cpp
@@ -0,0 +1,15 @@
+//https://codeforces.com/problemset/problem/959/A
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n=0;
+    cin >> n;
+
+    if(n%2==0)
+        cout << "Mahmoud" << endl;
+    else
+        cout << "Ehab" << endl;
+    return 0;
+}
