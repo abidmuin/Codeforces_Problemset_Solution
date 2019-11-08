9b147657945e38796b7fa1f3c515821f3d285f6d
Abid Hasan Muin <muin.739@gmail.com>
2019-11-08 20:39:45 +0600

    Add files via upload

---
+++ Solved using C++/Drinks/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n=0;    //No of oranges
+    cin >> n;
+
+    double total=0.0, partial=0.0;
+    for(int i=0; i<n; i++){
+        cin >> partial;
+        total = total + partial;
+    }
+    double percentage=0.0;
+    percentage = total / n;
+
+    cout << percentage << endl;
+    return 0;
+}
