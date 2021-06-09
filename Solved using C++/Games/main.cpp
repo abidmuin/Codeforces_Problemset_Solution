6fc2394d50e6a29bb6f3284497bf13eeda7e4da3
Abid Hasan Muin <muin.739@gmail.com>
2021-06-09 09:56:23 +0600

    Add files via upload

--- Solved using C++/Games/main.cpp
+++ Solved using C++/Games/main.cpp
@@ -9,13 +9,14 @@ int main() {
     cin >> n;
 
     int h[n]= {0}, a[n]= {0};
-    for(int i=0; i<n; i++)
-        cin >> h[i] >> a[i];
-
     int no_of_games=0;
+
     for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            if(h[i] == a[j])
+        cin >> h[i] >> a[i];
+        for(int j=0; j<i; j++) {
+            if(h[i]==a[j])
+                no_of_games++;
+            if(h[j]==a[i])
                 no_of_games++;
         }
     }
