fb6dd25e3ca67d57e6d092bfc289aacebe8bc8e2
Abid Hasan Muin <muin.739@gmail.com>
2021-06-28 20:01:10 +0600

    Add files via upload

---
+++ New Year and Hurry/cmake-build-debug/CMakeFiles/New_Year_and_Hurry.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/New_Year_and_Hurry.dir/main.cpp.obj"
+  "New_Year_and_Hurry.exe"
+  "New_Year_and_Hurry.exe.manifest"
+  "New_Year_and_Hurry.pdb"
+  "libNew_Year_and_Hurry.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/New_Year_and_Hurry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
